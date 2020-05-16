5ced2dde5c4de8f6d4898e3a77c7f38cc23e9791
Leah <LeahKahwadjian@gmail.com>
2020-05-16 17:02:54 -0400

    Updates

---
+++ precache-manifest.cd45ba839dfea0c9d46c51e26f2db785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936b4125abd1ae839c330d225fff5a7c",
+    "url": "/youtube-api/index.html"
+  },
+  {
+    "revision": "da90533266631594df6e",
+    "url": "/youtube-api/static/css/main.98ca0453.chunk.css"
+  },
+  {
+    "revision": "7027b5d74310bcf313b8",
+    "url": "/youtube-api/static/js/2.ae9233dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-api/static/js/2.ae9233dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da90533266631594df6e",
+    "url": "/youtube-api/static/js/main.f4215f16.chunk.js"
+  },
+  {
+    "revision": "157ebebbe54883eca9d6",
+    "url": "/youtube-api/static/js/runtime-main.a36f0382.js"
+  }
+]);
