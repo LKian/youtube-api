6c47955d94e948ad067d3a64a78d28a0a194f282
Leah <leahkahwadjian@gmail.com>
2020-05-16 10:08:04 -0400

    configured axios, made fetch request, linked to envariables variables, continued building Search + VideoList components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,45 @@
 import React from "react";
 import Search from "./Search";
+import youtube from "../APIs/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const APP_KEY = process.env.REACT_APP_API_KEY;
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: APP_KEY,
+      },
+    });
+    console.log(response.data.items);
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
-      <div>
-        <Search />
+      <div className="ui container">
+        <Search onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
 }
 
 export default App;
+
+// onTermSubmit = async term => {
+//     const response = await youtube.get("/search", {
+//       params: {
+//         q: term,
+//         part: "snippet",
+//         maxResults: 5,
+//         type: 'video',
+//         key: KEY
+//       }
+//     });
